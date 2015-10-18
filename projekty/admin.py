35cb0a320621b657324b3645a0cc26f590cf1a07
tomcio1205 <tomekrajzer@gmail.com>
2015-10-18 19:52:15 +0200

    admin_changes1

--- projekty/admin.py
+++ projekty/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id_prowadzacego','nazwisko', 'imie', 'tytul','stanowisko')
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id_student', 'nazwisko', 'imie','tok_studiow')
+
+
+class PrzedmiotAdmin(admin.ModelAdmin):
+    list_display = ('id_przedmiotu','nazwa', 'id_prowadzacego', 'typ_zajec','tok_studiow')
+
+
+class ProjektAdmin(admin.ModelAdmin):
+    list_display = ('id_projektu','nazwa', 'id_przedmiotu', 'liczba_miejsc')
+
+
+class WybranypAdmin(admin.ModelAdmin):
+    list_display = ('id_projektu', 'id_student', 'akceptacja')
+
+
+admin.site.register(Prowadzacy,TeacherAdmin)
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Projekt,ProjektAdmin)
+admin.site.register(Przedmiot,PrzedmiotAdmin)
+admin.site.register(Wybrany_projekt,WybranypAdmin)
