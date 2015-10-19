c4fce8e5b0475cdafe227d419e1164e47446d298
tomcio1205 <tomekrajzer@gmail.com>
2015-10-19 16:15:58 +0200

    add static file

--- zapisy/settings.py
+++ zapisy/settings.py
@@ -89,3 +89,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
--- zapisy/urls.py
+++ zapisy/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = patterns('',
                        # Examples:
@@ -9,3 +11,7 @@ urlpatterns = patterns('',
                        url(r'^$', 'projekty.views.home', name='home'),
                        url(r'^admin/', include(admin.site.urls)),
                        )
+
+if settings.DEBUG:
+    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
